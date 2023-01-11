06cf87ae85c268ac1171a1a28425ca829cae581d
Masterbonk <adrianjuul123@gmail.com>
2023-01-11 11:50:36 +0100

    initial upload

---
+++ Assets/Networking/NetworkManagerUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Netcode;
+
+
+public class NetworkManagerUI : MonoBehaviour
+{
+    [SerializeField]private Button Server;
+    [SerializeField]private Button Host;
+    [SerializeField]private Button Client;
+
+    private void Awake(){
+
+        Server.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartServer();
+        });
+        Host.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartHost();
+        });
+        Client.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartClient();
+        });
+    }
+
+}
