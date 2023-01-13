e699d9972af839a655fa278dad372dff1dba63ad
BardunoWitchTheMagi <bardurndberg@gmail.com>
2023-01-13 17:42:51 +0100

    Disconnect button for local client

--- Assets/Networking/NetworkManagerUI.cs
+++ Assets/Networking/NetworkManagerUI.cs
@@ -10,7 +10,7 @@ public class NetworkManagerUI : MonoBehaviour
     [SerializeField]private Button Server;
     [SerializeField]private Button Host;
     [SerializeField]private Button Client;
-    [SerializeField]private InputField Ip;
+   
 
     private void Awake(){
 
@@ -23,6 +23,8 @@ public class NetworkManagerUI : MonoBehaviour
         Client.onClick.AddListener(() => {
             NetworkManager.Singleton.StartClient();
         });
+
+      
     }
 
 }
