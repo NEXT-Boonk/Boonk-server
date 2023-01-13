68f484e42edef1a8e703d54c1dbcd96c596dc805
BardunoWitchTheMagi <bardurndberg@gmail.com>
2023-01-13 16:54:37 +0100

    TekstField with charValidation

--- Assets/Networking/IpInput.cs
+++ Assets/Networking/IpInput.cs
@@ -5,22 +5,33 @@ using UnityEngine;
 using UnityEngine.UI;
 using Unity.Netcode.Transports.UTP;
 using TMPro;
+using System; 
+
 
 
 public class IpInput : MonoBehaviour
 {
     
     //public UnityTransport placeholderName;
+    [SerializeField]private TMP_InputField Ip;
+    [SerializeField]private TMP_InputField Port;
     private string Input;
     UnityTransport UT;
-    TMP_InputField textField;
+    TMP_InputField Ip1;
+    TMP_InputField Port1;
     
 
     // Start is called before the first frame update
     void Awake()
     {
         UT = GetComponent<UnityTransport>();
-        textField = FindObjectOfType<TMP_InputField>();
+        Port.characterLimit = 4;
+        Port.contentType = TMP_InputField.ContentType.IntegerNumber;
+        Port.characterValidation = TMP_InputField.CharacterValidation.Alphanumeric;
+        
+        //Ip1 =  GameObject.Find("Ip").GetComponent<TMP_InputField>();
+        //Port1 =  GameObject.Find("Port").GetComponent<TMP_InputField>();
+        //textField = FindObjectOfType<TMP_InputField>();
 
     }
 
@@ -28,7 +39,11 @@ public class IpInput : MonoBehaviour
     void Update()
     {
 
-        UT.ConnectionData.Address = textField.text;
+        UT.ConnectionData.Address = Ip.text;
+        if(Port.text.Length > 0 && Port.text.Length <= 4){
+        UT.ConnectionData.Port = UInt16.Parse(Port.text);
+       }
+       // UT.ConnectionData.Port = Port.text;
       //if (Input.GetKey(KeyCode.G)) UT.connectionData.Address = "10.78.64.202";
         
     }
