0a242d0dd1e3e48c54da256b99e5c72410722d36
Masterbonk <adrianjuul123@gmail.com>
2023-01-25 13:02:15 +0100

    Adding custom struct class type

--- Assets/Networking/PlayerNetwork.cs
+++ Assets/Networking/PlayerNetwork.cs
@@ -7,18 +7,57 @@ using UnityEngine.UI;
 public class PlayerNetwork : NetworkBehaviour
 {
 
-
     private NetworkVariable<int> randomNumber = new NetworkVariable<int>(1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
 
 
+//Dette er en struct, den minder om en class, men er det ikke
+    public struct MyCustomData {
+        public int _int;
+        public bool _bool;
+
+    }
+
+        private NetworkVariable<MyCustomData> customNumber = new NetworkVariable<MyCustomData>(
+        new MyCustomData{
+            _int = 51,
+            _bool = true,
+        }, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+/*Denne del af koden vil kun sende det tilfældige nummer når nummeret ændrer sig, i stedet for hele tiden
+    public override void OnNetworkSpawn() {
+        randomNumber.OnValueChanged += (int previousValue, int newValue) => {
+            Debug.Log(OwnerClientId + "number: " + randomNumber.Value);
+        };
+    }
+    */
+    public override void OnNetworkSpawn() {
+        customNumber.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) => {
+        Debug.Log(OwnerClientId + "; " + newValue._int + " and it's " + newValue._bool);
+
+        };
+    }
 
     private void Update() {
-        Debug.Log(OwnerClientId + "number: " + randomNumber.Value);
         if(!IsOwner) return;
+        Debug.Log(OwnerClientId + "number: " + randomNumber.Value);
+
 
         if(Input.GetKeyDown(KeyCode.T)){
             randomNumber.Value = Random.Range(0,100);
+        }
+        if(Input.GetKeyDown(KeyCode.Y)){
+            if(customNumber.Value._int == 51){
+            customNumber.Value = new MyCustomData{
+                _int = 10,
+                _bool = false,
+            };
+            } else {
+            customNumber.Value = new MyCustomData{
+                _int = 51,
+                _bool = true,
+            };
 
+            }
         }
 
         Vector3 moveDir = new Vector3(0,0,0);
