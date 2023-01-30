bc3f5577df4b196c5d3d45887115b72c9ee49c11
BardunoWitchTheMagi <bardurndberg@gmail.com>
2023-01-30 10:47:18 +0100

    confirm button

--- Assets/Networking/IpInput.cs
+++ Assets/Networking/IpInput.cs
@@ -12,34 +12,31 @@ using System;
 public class IpInput : MonoBehaviour
 {
     
-    //public UnityTransport placeholderName;
-    [SerializeField]private TMP_InputField Ip; // laver et felt i Scriptet for et Input field(text mesh pro)
+    [SerializeField]private TMP_InputField Ip; // makes a field in the Unity editor for the Input field(text mesh pro)
     [SerializeField]private TMP_InputField Port;
     private string Input;
     UnityTransport UT;
     
 
-    // Start is called before the first frame update
     void Awake()
     {
-        UT = GetComponent<UnityTransport>(); // finder UnityTransport
-        Port.characterLimit = 5; // gør at input feltet "Port" har en char limit på 5 
-        Port.contentType = TMP_InputField.ContentType.IntegerNumber; // gør at input feltet "Port" kun kan bruge Integers
-        Port.characterValidation = TMP_InputField.CharacterValidation.Alphanumeric; // gør at input feltet "Port" kun kan accepterer Integers
+        UT = GetComponent<UnityTransport>(); // finds the UnityTransport component
+        Port.characterLimit = 4; // makes the character limit of the input field 4
+        Port.contentType = TMP_InputField.ContentType.IntegerNumber; // makes the content of the input feild only allow Integer numbers
+        Port.characterValidation = TMP_InputField.CharacterValidation.Alphanumeric; // makes the port only accept Alphanumeric values
         
 
     }
         
     public void InputIp(){
 
-        UT.ConnectionData.Address = Ip.text; //sætter stringen i Inputfeltet "Ip" til Ip adressen programmet bruger
+        UT.ConnectionData.Address = Ip.text; //sets the Ip to the string in the input field
         
     }
     
     public void InputPort(){
     if(Port.text.Length > 0 && Port.text.Length <= 4){
-        UT.ConnectionData.Port = UInt16.Parse(Port.text); //sætter stringen i Inputfeltet "Port" og parser den til en UInt16 som programmet bruger
-        
+        UT.ConnectionData.Port = UInt16.Parse(Port.text); //sets the Port to a 4 digit number        
     }
 }
 }
